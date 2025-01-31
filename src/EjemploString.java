6b76cc815fc43ecf23f878a39905038e8576dabc
LexUribe <alex.uribe@hotmail.com>
2025-01-31 11:41:08 -0500

    Se eliminan clases que no pertenecen a la rama y se crea la primera clase de esta rama.

---
+++ src/EjemploString.java
@@ -0,0 +1,8 @@
+public class EjemploString {
+    public static void main(String[] args) {
+        String curso = "Programación Java.";
+        String curso1 = new String("Programación Java.");
+
+    }
+}
+
