9e23cb590ede2bf32b224588b3198e99a4494bcb
LexUribe <alex.uribe@hotmail.com>
2025-02-01 00:32:02 -0500

    Se agregan ejemplos basicos de un String y como concatenar con números.

--- src/EjemploString.java
+++ src/EjemploString.java
@@ -1,8 +1,26 @@
 public class EjemploString {
     public static void main(String[] args) {
-        String curso = "Programación Java.";
-        String curso1 = new String("Programación Java.");
+        //Objeto String en la literal vs operador.
+        String curso = "Programación Java."; //Método estándar (Forma literal) abreviado por ser una clase muy usada.
+        String curso1 = new String("Programación Java."); //Método tradicional con el constructor.
+        String curso2 = "programación java.";
 
+        System.out.println("curso = " + curso);
+        System.out.println("curso1 = " + curso1);
+        System.out.println("curso2 = " + curso2);
+
+        boolean esIgual = curso == curso1; //Comparación por referencia con el "==".
+        System.out.println("\n\"curso == curso1\" = " + esIgual);
+
+        esIgual = curso.equals(curso1); //Comparación por valor con el método "equals()" de la clase String.
+        System.out.println("\"curso.equals(curso1)\" = " + esIgual);
+
+        esIgual = curso.equalsIgnoreCase(curso2); //Hace la comparación ignorando las mayúsculas.
+        System.out.println("\"curso.equalsIgnoreCase(curso2)\" = " + esIgual);
+
+        String curso3 = "Programación Java."; //Java optimiza la memoria detectando que los objetos iguales "curso == curso3".
+        System.out.println("\ncurso3 = " + curso3);
+        esIgual = curso == curso3;
+        System.out.println("\"curso == curso3\" = " + esIgual);
     }
 }
-
---
+++ src/EjemploStringConcatenacion.java
@@ -0,0 +1,24 @@
+public class EjemploStringConcatenacion {
+    public static void main(String[] args) {
+        //Concatenación de objetos de la clase String.
+        //Concatenación cadenas de texto.
+        String curso = "Programación Java"; //Método estándar (Forma literal) abreviado por ser una clase muy usada.
+        String profesor = "Alvaro Uribe ParacoHP";
+
+        String detalle = curso + " con el instructor " + profesor;
+        System.out.println(detalle);
+
+        //Concatenación cadenas de texto y números enteros.
+        int numeroA = 10;
+        int numeroB = 50;
+        System.out.println(detalle + numeroA + numeroB); //Si al principio se encuentra una cadena asume que se trata de una cadena en su totalidad. Las expresiones se evalúan de izquierda a derecha.
+        System.out.println(detalle + (numeroA + numeroB)); //Los paréntesis "()" cambian la precedencia o el orden en que se ejecutan las operaciones.
+        System.out.println(numeroA + numeroB + detalle);
+
+        String detalle1 = curso.concat(" con ".concat(profesor));
+        System.out.println("detalle1 = " + detalle1);
+
+        detalle1 = curso.concat(" con ").concat(profesor);
+        System.out.println("detalle1.1 = " + detalle1);
+    }
+}
