9f45d416a4c9b0aa3c6d0eab2aa10d8994d65e44
LexUribe <alex.uribe@hotmail.com>
2025-06-29 16:58:26 -0500

    Tareas 1 y 2 del curso con su rama.

---
+++ src/Tarea1.java
@@ -0,0 +1,17 @@
+public class Tarea1 {
+    public static void main(String[] args) {
+        /*
+         * Tarea 1: Imprime tu nombre en consola. SECCIÓN 1: INTRODUCCIÓN.
+         *
+         * El objetivo de esta primera tarea es crear una nueva clase con el método main y dentro imprime tu
+         * nombre en la salida estándar o consola utilizando System.out.println.
+         *
+         * Aunque parece simple, es importante desde el principio conocer bien la clase main y la salida
+         * estándar de java en consola.
+         * */
+
+        String nombre = "Alexander Uribe";
+
+        System.out.println("¡Mucho gusto!, mi nombre es " + nombre + ".");
+    }
+}
